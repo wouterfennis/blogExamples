70ad7b02698910d48bd82bda590d9ccb1171e9b9
wouter_hilversum@hotmail.com <wouter_hilversum@hotmail.com>
2024-06-10 21:27:04 +0200

    Working example

--- ClientCertificates/WebApi/GenerateCertificates.ps1
+++ ClientCertificates/WebApi/GenerateCertificates.ps1
@@ -6,13 +6,13 @@ if($isAdmin -eq $false){
 }
 
 # Create Root CA
-$rootCert = New-SelfSignedCertificate -DnsName "RootCA" -CertStoreLocation "cert:\LocalMachine\My" -NotAfter (Get-Date).AddYears(10) -KeyUsage CertSign, DigitalSignature -KeySpec Signature -HashAlgorithm SHA256
+$rootCert = New-SelfSignedCertificate -DnsName "RootCA" -CertStoreLocation "cert:\LocalMachine\My" -NotAfter (Get-Date).AddYears(10) -KeyUsage CertSign, DigitalSignature -KeySpec Signature -HashAlgorithm SHA256 -FriendlyName "Blog Root certificate"
 
 # Export Root CA to .cer file
 $rootCert | Export-Certificate -FilePath "RootCA.cer"
 
 # Create Client Certificate
-$clientCert = New-SelfSignedCertificate -DnsName "ClientCert" -CertStoreLocation "cert:\LocalMachine\My" -NotAfter (Get-Date).AddYears(1) -Signer $rootCert -KeyUsage DigitalSignature -HashAlgorithm SHA256 -TextExtension @('2.5.29.37={text}1.3.6.1.5.5.7.3.2')
+$clientCert = New-SelfSignedCertificate -DnsName "ClientCert" -CertStoreLocation "cert:\CurrentUser\My" -NotAfter (Get-Date).AddYears(1) -Signer $rootCert -KeyUsage DigitalSignature -HashAlgorithm SHA256 -TextExtension @('2.5.29.37={text}1.3.6.1.5.5.7.3.2') -FriendlyName "Blog client certificate"
 
 # Export Client Certificate to .pfx file (with private key)
 $clientCert | Export-PfxCertificate -FilePath "ClientCert.pfx" -Password (ConvertTo-SecureString -String "your_password" -Force -AsPlainText)
@@ -21,3 +21,7 @@ $clientCert | Export-PfxCertificate -FilePath "ClientCert.pfx" -Password (Conver
 $clientCert | Export-Certificate -FilePath "ClientCert.cer"
 
 Write-Host "Generated RootCA.cer, ClientCert.pfx, and ClientCert.cer"
+
+Import-Certificate -FilePath "RootCA.cer" -CertStoreLocation "cert:\LocalMachine\Root"
+
+Write-Host "Imported RootCA.cer to root"
--- ClientCertificates/WebApi/Program.cs
+++ ClientCertificates/WebApi/Program.cs
@@ -1,6 +1,7 @@
-
 using Microsoft.AspNetCore.Authentication.Certificate;
 using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.AspNetCore.Server.Kestrel.Https;
 using System.Security.Claims;
 using System.Security.Cryptography.X509Certificates;
 
@@ -11,6 +12,15 @@ namespace WebApi
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            // Request client certificates and require them for all requests
+            builder.Services.Configure<KestrelServerOptions>(options =>
+            {
+                options.ConfigureHttpsDefaults(options =>
+                {
+                    options.ClientCertificateMode = ClientCertificateMode.RequireCertificate;
+                });
+            });
 
             // Add services to the container.
 
@@ -35,7 +45,7 @@ namespace WebApi
                 options.KnownProxies.Clear();
             }).AddCertificateForwarding(options =>
             {
-                options.CertificateHeader = "X-ARR-ClientCert"; // This is standard with App Services
+                //options.CertificateHeader = "X-ARR-ClientCert"; // This is standard with App Services
 
                 options.HeaderConverter = (headerValue) =>
                 {
@@ -51,7 +61,7 @@ namespace WebApi
             .AddCertificate(options =>
             {
                 options.AllowedCertificateTypes = CertificateTypes.All;
-                options.RevocationMode = X509RevocationMode.Online;
+                options.RevocationMode = X509RevocationMode.NoCheck;
                 options.ValidateCertificateUse = true;
                 options.ValidateValidityPeriod = true;
                 options.Events = new CertificateAuthenticationEvents
@@ -68,6 +78,7 @@ namespace WebApi
                         else
                         {
                             context.Fail("Invalid certificate");
+                            Console.WriteLine("Invalid certificate");
                         }
 
                         return Task.CompletedTask;
@@ -89,6 +100,8 @@ namespace WebApi
             // This will make the client certificate available to the application
             app.UseForwardedHeaders();
             app.UseCertificateForwarding();
+
+            app.UseAuthentication();
 
             app.UseHttpsRedirection();
 
